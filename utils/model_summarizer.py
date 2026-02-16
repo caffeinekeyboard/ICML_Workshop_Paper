8c8382b1065d867447dacb06c4f84245bd86d384
caffeinekeyboard <bhaveaarya09@gmail.com>
2026-02-16 21:11:03 +0530

    add: utils.model_summarizer, assets/gumnet_2d_architecture_summary.txt

---
+++ utils/model_summarizer.py
@@ -0,0 +1,17 @@
+import torch
+from torchinfo import summary
+from model.gumnet import GumNet
+
+model = GumNet()
+input_shapes = [(2, 1, 192, 192), (2, 1, 192, 192)]
+print("\n" + "="*50)
+print("GUMNET 2D ARCHITECTURE SUMMARY")
+print("="*50)
+
+summary(
+    model, 
+    input_size=input_shapes,
+    col_names=["input_size", "output_size", "num_params", "trainable"],
+    col_width=20,
+    row_settings=["var_names"]
+)
